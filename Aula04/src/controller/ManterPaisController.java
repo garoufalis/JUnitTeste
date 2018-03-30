db16459fb0ba0874ed544258fa2b30dca9f9f10c
garoufa <garoufa@garoufa>
2018-03-29 21:45:45 -0300

    Aula04

---
+++ Aula04/src/controller/ManterPaisController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pais;
+import service.PaisService;
+
+/**
+ * Servlet implementation class ManterPaisController
+ */
+@WebServlet("/ManterPais.do")
+public class ManterPaisController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pNome = request.getParameter("Nome");
+		String pPopulacao = request.getParameter("Populacao");
+		String pArea = request.getParameter("Area");
+		
+		Pais pais = new Pais();
+		pais.setNome(pNome);
+		pais.setPopulacao(Long.parseLong(pPopulacao));
+		pais.setArea(Double.parseDouble(pArea));
+		
+		PaisService ps = new PaisService();
+		ps.criar(pais);
+		pais = ps.carregar(pais.getId());
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Cliente Cadastrado</title></head><body>");
+		out.println(	"id: " + pais.getId()+"<br>");
+		out.println(	"nome: " + pais.getNome()+"<br>");
+		out.println(	"fone: " + pais.getPopulacao()+"<br>");
+		out.println(	"e-mail: " + pais.getArea()+"<br>");
+	    out.println("</body></html>");
+	}
+
+}
