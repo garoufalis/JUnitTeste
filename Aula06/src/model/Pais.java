fad405a9b537d791b109165bb88e1d447653610b
garoufa <garoufa@garoufa>
2018-04-07 19:14:38 -0300

    Aula06

---
+++ Aula06/src/model/Pais.java
@@ -0,0 +1,70 @@
+package model;
+import java.io.Serializable;
+
+public class Pais implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String nomePais;
+	private long populacao;
+	private double area;
+	
+	public int getId() {
+		return id;
+	}
+	public String getNome() {
+		return nomePais;
+	}
+	public long getPopulacao() {
+		return populacao;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setNome(String nomePais) {
+		this.nomePais = nomePais;
+	}
+	public void setPopulacao(long populacao) {
+		this.populacao = populacao;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	
+	@Override
+    public String toString() {
+        return "Pais [id=" + id + ", nome=" + nomePais + ", populacao=" + populacao
+                + ", area=" + area + "]";
+    }
+ 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pais other = (Pais) obj;
+        if (area == -1) {
+            if (other.area != -1)
+                return false;
+        } else if (area != other.area)
+            return false;
+        if (populacao == -1) {
+            if (other.populacao != -1)
+                return false;
+        } else if (populacao != other.populacao)
+            return false;
+        if (id != other.id)
+            return false;
+        if (nomePais == null) {
+            if (other.nomePais != null)
+                return false;
+        } else if (!nomePais.equals(other.nomePais))
+            return false;
+        return true;
+    }
+}
